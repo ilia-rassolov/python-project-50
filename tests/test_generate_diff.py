f4637b9d93cdb4406a955d939006fea34137ea77
ilia <iliarassolov@gmail.com>
2024-05-15 09:29:18 +0400

    check 1.3

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -6,6 +6,8 @@ import pytest
     ('tests/fixtures/nest_f1.json', 'tests/fixtures/nest_f2.json', 'stylish'),
     ('tests/fixtures/nest_f1.yaml', 'tests/fixtures/nest_f2.yaml', 'stylish'),
     ('tests/fixtures/nest_f1.json', 'tests/fixtures/nest_f2.json', 'plain'),
+    ('tests/fixtures/nest_f1.yml', 'tests/fixtures/nest_f2.json', 'plain'),
+    ('tests/fixtures/nest_f1.yml', 'tests/fixtures/nest_f2.yaml', 'json'),
     ('tests/fixtures/nest_f1.yaml', 'tests/fixtures/nest_f2.yaml', 'json')])
 def test_generate_diff(filepath1, filepath2, formatter):
 
